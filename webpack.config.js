d4fd3fa3d802e12d37d1ac8dd48d20ad8fac66c9
nikunj <nikunj.pansuriya@freestoneinfotech.com>
2020-04-05 19:05:15 +0530

    Added react setup config.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+
+  mode: 'development',
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8888
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
+}
